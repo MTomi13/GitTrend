9754eb588c329071d9bc26ac536e76ffb2e2907f
Tamas Marton <tamas.marton@nedap.com>
2018-11-09 17:44:10 +0100

    remove RxAndroid and use coroutines Dispatchers.Main

--- app/src/main/java/tamas/marton/gittrend/api/schedulers/DispatcherProviderImpl.kt
+++ app/src/main/java/tamas/marton/gittrend/api/schedulers/DispatcherProviderImpl.kt
@@ -1,14 +1,14 @@
 package tamas.marton.gittrend.api.schedulers
 
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.rx2.asCoroutineDispatcher
 
 
 class DispatcherProviderImpl : DispatcherProvider {
 
-    override fun mainThread(): CoroutineDispatcher = AndroidSchedulers.mainThread().asCoroutineDispatcher()
+    override fun mainThread(): CoroutineDispatcher = Dispatchers.Main
 
     override fun backgroundThread(): CoroutineDispatcher = Schedulers.io().asCoroutineDispatcher()
 
